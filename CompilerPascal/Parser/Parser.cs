b883fc9990f94df48850f4a45aa22744f77ceedd
IlonaKalinina <ilonamin2000@gmail.com>
2022-12-18 19:02:59 +1000

    18/12

--- CompilerPascal/Parser/Parser.cs
+++ CompilerPascal/Parser/Parser.cs
@@ -16,14 +16,14 @@ namespace CompilerPascal.Parser
         private readonly Lexer.Lexer lexer;
         private bool findError = false;
 
-       public Parser(string fileName)
+        public Parser(string fileName)
         {
             lexer = new Lexer.Lexer(fileName);
             nowLexem = lexer.GetLexem();
 
             doParse = Expression(nowLexem.valueLexema.ToString());
 
-            if(nowLexem.categoryLexeme != "End File" && !findError)
+            if (nowLexem.categoryLexeme != "End File" && !findError)
             {
                 doParse = new Node()
                 {
@@ -33,123 +33,21 @@ namespace CompilerPascal.Parser
             }
         }
 
-        public void Programm(string input_data)
-        {
-            if (nowLexem.valueLexema.ToString() == "programm")
-            {
-                //добавить программ в корень
-                //ищем следующую лексему, чтоб найти название программы
-                nowLexem = lexer.GetLexem(); //добавить проверку что не конец файла
-                if (nowLexem.categoryLexeme.ToString() == "identifier")
-                {
-                    nowLexem = lexer.GetLexem();
-                    if (nowLexem.categoryLexeme == "separators" && nowLexem.valueLexema.ToString() == ";")
-                    {
-                    }
-                    else
-                    {
-                        //ошибка, нет ;
-                    }
-                }
-                else
-                {
-                    //ошибка, не может не быть названия
-                }
-            }
-            //если нет программ, то нет ошибки, но нужно добавить в корень просто программ без названия, далее ищем описание программы var (ребенок)
-            Var(input_data);
-        }
-        public void Var(string input_data)
-        {
-            //ищем ключевое слово
-            nowLexem = lexer.GetLexem();
-            if (nowLexem.valueLexema.ToString() == "var")
-            {
-                //записываем куда то вар
-                //ищем идентификаторы
-                Id(input_data);
-            }
-            else
-            {
-                //ошибка
-            }
-        }
-        //a, b, c
-        public void Id(string input_data)
-        {
-            nowLexem = lexer.GetLexem();
-            while (nowLexem.valueLexema.ToString() != ":")
-            {
-                if (nowLexem.categoryLexeme.ToString() == "indetifier")
-                {
-                    //говорим что нашли одного ребенка вар, далее ищем либо еще одного, либо :
-                    nowLexem = lexer.GetLexem();
-                    if (nowLexem.valueLexema.ToString() == ",")
-                    {
-                        Id(input_data);
-                    }
-                }
-            }
-            //после нахождения : ищем тип данных
-        }
-
-        public void Type(string input_data)
-        {
-            nowLexem = lexer.GetLexem();
-            if (nowLexem.valueLexema.ToString() == "identifier" ||
-                nowLexem.valueLexema.ToString() == "string" ||
-                nowLexem.valueLexema.ToString() == "real" ||
-                nowLexem.valueLexema.ToString() == "keyword" ||
-                nowLexem.valueLexema.ToString() == "char" ||
-                nowLexem.valueLexema.ToString() == "integer")
-            {
-                //записываем тип данных
-                nowLexem = lexer.GetLexem();
-                if (nowLexem.categoryLexeme == "separators" && nowLexem.valueLexema.ToString() == ";")
-                {
-                }
-            }
-            else
-            {
-                //ошибка, не указан тип данных
-            }
-        }
-
         public Node Expression(string input_data)
         {
-        Node leftСhild = Term(input_data);
-        if (leftСhild.category != "error")
-        {
-            if (nowLexem.categoryLexeme != "End File")
+            Node leftСhild = Term(input_data);
+            if (leftСhild.category != "error")
             {
-                while (nowLexem.valueLexema.ToString() == "+" || nowLexem.valueLexema.ToString() == "-")
+                if (nowLexem.categoryLexeme != "End File")
                 {
-                    var BinOp = nowLexem.valueLexema;
-                    if (nowLexem.categoryLexeme != "End File")
+                    while (nowLexem.valueLexema.ToString() == "+" || nowLexem.valueLexema.ToString() == "-")
                     {
-                        nowLexem = lexer.GetLexem();
+                        leftСhild = AddChilde(leftСhild, input_data);
+                        if (nowLexem.categoryLexeme == "End File") break;
                     }
-                    Node rightСhild = Term(input_data);
-                    leftСhild = new Node()
-                    {
-                        category = "binOp",
-                        value = BinOp,
-                        children = new List<Node> { leftСhild, rightСhild }
-                    };
-                    if (rightСhild.category == "error")
-                    {
-                        return new Node()
-                        {
-                            category = "error",
-                            value = rightСhild.value
-                        };
-                    }
-
-                    if (nowLexem.categoryLexeme == "End File") break;
                 }
             }
-        }
-        return leftСhild;
+            return leftСhild;
         }
 
         public Node Term(string input_data)
@@ -159,37 +57,9 @@ namespace CompilerPascal.Parser
             {
                 if (nowLexem.categoryLexeme != "End File")
                 {
-                    nowLexem = lexer.GetLexem();
-                }
-                if (nowLexem.categoryLexeme != "End File")
-                {
                     while (nowLexem.valueLexema.ToString() == "*" || nowLexem.valueLexema.ToString() == "/")
                     {
-                        var BinOp = nowLexem.valueLexema;
-                        if (nowLexem.categoryLexeme != "End File")
-                        {
-                            nowLexem = lexer.GetLexem();
-                        }
-                        Node rightСhild = Factor(input_data);
-                        if (nowLexem.categoryLexeme != "End File")
-                        {
-                            nowLexem = lexer.GetLexem();
-                        }
-                        
-                        leftСhild = new Node()
-                        {
-                            category = "binOp",
-                            value = BinOp,
-                            children = new List<Node> { leftСhild, rightСhild }
-                        };
-                        if (rightСhild.category == "error")
-                        {
-                            return new Node()
-                            {
-                                category = "error",
-                                value = rightСhild.value
-                            };
-                        }
+                        leftСhild = AddChilde(leftСhild, input_data);
                         if (nowLexem.categoryLexeme == "End File") break;
                     }
                 }
@@ -201,44 +71,76 @@ namespace CompilerPascal.Parser
         {
             if (nowLexem.categoryLexeme == "integer" || nowLexem.categoryLexeme == "real")
             {
-                return new Node()
+                Node nowChilde = new Node()
                 {
                     category = "number",
                     value = nowLexem.valueLexema
                 };
+                if (nowLexem.categoryLexeme != "End File") nowLexem = lexer.GetLexem();
+                return nowChilde;
             }
+
             if (nowLexem.categoryLexeme == "identifier")
             {
-                return new Node()
+                Node nowChilde = new Node()
                 {
                     category = "identifier",
                     value = nowLexem.valueLexema
                 };
+                if (nowLexem.categoryLexeme != "End File") nowLexem = lexer.GetLexem();
+                return nowChilde;
             }
 
             if (nowLexem.valueLexema.ToString() == "(")
             {
-                Node nextExpression = new Node();
-                if (nowLexem.categoryLexeme != "End File")
+                nowLexem = lexer.GetLexem();
+                Node nextExpression = Expression(input_data);
+
+                if (nowLexem.categoryLexeme == "End File")
+                {
+                    findError = true;
+                    Node nowChilde = new Node()
+                    {
+                        category = "error",
+                        value = $"No right bracket on line {nowLexem.numberLine - 1}"
+                    };
+                    return nowChilde;
+                }
+                else
                 {
                     nowLexem = lexer.GetLexem();
-
-                    nextExpression = Expression(input_data);
-
-                    if (nowLexem.categoryLexeme == "End File")
-                    {
-                        findError = true;
-                        return new Node()
-                        {
-                            category = "error",
-                            value = $"No right bracket on line {nowLexem.numberLine - 1}"
-                        };
-                    }
-
+                    return nextExpression;
                 }
-                return nextExpression;
             }
+            return Exep();
+        }
 
+        public Node AddChilde(Node leftСhild, string input_data)
+        {
+            var BinOp = nowLexem.valueLexema;
+            if (nowLexem.categoryLexeme != "End File")
+            {
+                nowLexem = lexer.GetLexem();
+            }
+            leftСhild = new Node()
+            {
+                category = "binOp",
+                value = BinOp,
+                children = new List<Node> { leftСhild, Term(input_data) }
+            };
+            if (leftСhild.children[1].category == "error")
+            {
+                return new Node()
+                {
+                    category = "error",
+                    value = leftСhild.children[1].value
+                };
+            }
+            return leftСhild;
+        }
+
+        public Node Exep()
+        {
             if (nowLexem.valueLexema != null)
             {
                 findError = true;
@@ -247,7 +149,8 @@ namespace CompilerPascal.Parser
                     category = "error",
                     value = $"Syntax error on line {nowLexem.numberLine}, don't have factor"
                 };
-            } else
+            }
+            else
             {
                 findError = true;
                 return new Node()
@@ -256,7 +159,6 @@ namespace CompilerPascal.Parser
                     value = $"Syntax error on line {nowLexem.numberLine - 1}, don't have factor"
                 };
             }
-               
         }
     }
 }
--- CompilerPascal/Program.cs
+++ CompilerPascal/Program.cs
@@ -47,15 +47,15 @@ namespace CompilerPascal
                         }
                     }
                 }
-                //if (args.Contains("-pars"))
-                //{
-                    //Parser.Parser P = new Parser.Parser(args[0]);
-                    Parser.Parser P = new Parser.Parser(@"..\..\..\..\CompilerPascal.Test\Tests\ParserTest\Files\500.in");
+                if (args.Contains("-pars"))
+                {
+                    Parser.Parser P = new Parser.Parser(args[0]);
+                    //Parser.Parser P = new Parser.Parser(@"..\..\..\..\CompilerPascal.Test\Tests\ParserTests\Files\1.in");
 
                     List<string> answer = new List<string>();
 
                     RunTree(P.doParse);
-                //}
+                }
             }
             catch
             {
